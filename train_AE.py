9bdefb8964fa61fd7ebd4d329ec137f3777ffdbc
JuyaoHu <hujunyao0329@gmail.com>
2024-11-24 23:55:28 +0800

    update

--- train_AE.py
+++ train_AE.py
@@ -23,7 +23,7 @@ save_dir = "/home/pod/shared-nvme/tensorboard/logs/OCT_AE"
 dataset = TrainDataset("/home/pod/shared-nvme/data/EyeOCT/train", width=256)
 dataloader = DataLoader(dataset, num_workers=14, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=500, max_images=8, increase_log_steps=True)
-trainer = pl.Trainer(strategy='ddp', gpus=2, precision=32, callbacks=[logger], default_root_dir=save_dir)
+trainer = pl.Trainer(strategy='ddp', gpus=2, precision=32, callbacks=[logger], default_root_dir=save_dir, max_steps=-1)
 # trainer = pl.Trainer(gpus=1, callbacks=[logger])
 trainer.fit(model, dataloader)
 
--- train_DM.py
+++ train_DM.py
@@ -7,7 +7,7 @@ from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
 
 # Configs
-resume_path = None
+resume_path = "/home/pod/shared-nvme/tensorboard/logs/OCT_DM/lightning_logs/version_19/checkpoints/epoch=41-step=1973.ckpt"
 batch_size = 2
 learning_rate = 4e-5
 ddim_steps = 10
@@ -25,7 +25,7 @@ save_dir = "/home/pod/shared-nvme/tensorboard/logs/OCT_DM"
 dataset = TrainDataset("/home/pod/shared-nvme/data/EyeOCT/train")
 dataloader = DataLoader(dataset, num_workers=14, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=500, max_images=8, increase_log_steps=True, log_images_kwargs=dict(ddim_steps=ddim_steps))
-trainer = pl.Trainer(strategy='ddp', gpus=2, precision=32, callbacks=[logger], default_root_dir=save_dir, accumulate_grad_batches=accumulate)
+trainer = pl.Trainer(strategy='ddp', gpus=2, precision=32, callbacks=[logger], default_root_dir=save_dir, accumulate_grad_batches=accumulate, max_steps=-1)
 # trainer = pl.Trainer(gpus=1, callbacks=[logger])
 trainer.fit(model, dataloader)
 
